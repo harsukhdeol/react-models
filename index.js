ae9b777add7926a76136ce4c12e4265a5f40a87b
sukhd <centaurgolden@gmail.com>
2020-05-30 11:43:34 -0500

    connected components to index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { AppRegistry, StyleSheet, Text, View } from "react-360";
+import ModelView from "./components/ModelView";
+import TopPosts from "./components/TopPosts";
+import CurrentPost from "./components/CurrentPost";
 import * as Store from "./components/Store";
 Store.initialize(process.env.KEY);
 
-export default class react_model_viewer extends React.Component {
+/* export default class react_model_viewer extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
@@ -13,9 +16,9 @@ export default class react_model_viewer extends React.Component {
       </View>
     );
   }
-}
+} */
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
@@ -34,5 +37,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 });
+ */
 
-AppRegistry.registerComponent("react_model_viewer", () => react_model_viewer);
+//AppRegistry.registerComponent("react_model_viewer", () => react_model_viewer);
+
+AppRegistry.registerComponent("TopPosts", () => TopPosts);
+AppRegistry.registerComponent("CurrentPost", () => CurrentPost);
+AppRegistry.registerComponent("ModelView", () => ModelView);
