3b9db34e70563d41d8f2d84fb34d62df72e78df0
sukhd <centaurgolden@gmail.com>
2020-05-30 11:24:49 -0500

    init store functions

---
+++ components/Store.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+
+//create redux like store to sync all elements
+
+const State ={
+    posts: undefined,
+    current =-1
+}
+const listeners = new Set();//whats a set
+
+function updateComponents (){
+    for(const cb of listeners.values()){
+        cb()
+    }
+}
+
+const POLY_PATH = "https://poly.googleapis.com/v1/assets?";
+
+export function initialize (apiKey){
+    //fetch top 5 posts from google poly
+const options = {
+    curated:true,
+    format:'GLTF2', 
+    key: apiKey,
+    pageSize:5
+};
+const queryString = Object.keys(options)
+.map(k=>    `${k}=${options[k]}`)
+.join("&");
+
+console.log(queryString)
+fetch(POLY_PATH+queryString)
+.then(body=>{
+   const entries = body.assets.map(asset=>{
+const objSource = asset.formats.filter(
+    format=>format.formatType ==='GLTF2'
+)[0];
+return {
+    id:asset.name,
+    name:asset.displayName,
+    author:asset.authorName,
+    description: asset.description,
+    source:objSource,
+    preview:asset.thumbnail.url
+}
+    }) ;
+    State.posts = entries;
+    updateComponents();
+
+});
+}
+
+export function setCurrent (value){
+    State.current= value;
+    updateComponents();
+}
+export function connect (Component){
+    return class Wrapper extends React.Component{
+        state={
+            posts:State.posts,
+            current:State.current,
+        };
+        _listener=()=>{
+            this.setState({
+                posts:State.posts,
+                current:State.current,
+            });
+        };
+
+        componentDidMount(){
+            listeners.add(this._listener);
+        }
+
+        componentWillUnmount(){
+            listeners.delete(this._listener);
+        }
+
+        render(){
+            return(
+                <Component
+                {...this.props}
+                posts = {this.state.posts}
+                current = {this.state.current}
+                />
+            )
+        }
+
+    }
+}
--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import React from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-360";
+import * as Store from "./components/Store";
+Store.initialize(process.env.KEY);
 
 export default class react_model_viewer extends React.Component {
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
+          <Text style={styles.greeting}>Welcome to React 360</Text>
         </View>
       </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    justifyContent: "center",
+    alignItems: "center",
   },
   greetingBox: {
     padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
+    backgroundColor: "#000000",
+    borderColor: "#639dda",
     borderWidth: 2,
   },
   greeting: {
@@ -40,4 +35,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('react_model_viewer', () => react_model_viewer);
+AppRegistry.registerComponent("react_model_viewer", () => react_model_viewer);
